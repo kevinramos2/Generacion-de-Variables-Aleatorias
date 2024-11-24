1819fb19f9ed8eadaec2ae67fc353c60c5bf4fa0
kevinramos2 <keramosl@unal.edu.co>
2024-11-23 22:16:43 -0500

    Update main2.py

--- main2.py
+++ main2.py
@@ -2,7 +2,15 @@ import random
 import numpy as np
 import math
 
+#Función para cambiar de la normal (0,1) a la normal con parámetros (miu,sigma^2)
+def boxMuller(u1,u2):
+  x = (math.sqrt(-2*math.log(u1)))*math.cos(2*math.pi*u2)
+  y = (math.sqrt(-2*math.log(u1)))*math.sin(2*math.pi*u2)
+
+  return x,y
 #Ejercicio #2 - Ruta de correo
 u1 = random.random()
 u2 = random.random()
-        
+#Plaza de luces N(38,16)
+PlazaLuces = boxMuller(u1,u2)
+print(PlazaLuces)
