694c3773dbc5863e25ca5a03d47ed567d5249ceb
kevinramos2 <keramosl@unal.edu.co>
2024-11-27 23:06:01 -0500

    Update main3.py

--- main3.py
+++ main3.py
@@ -3,17 +3,23 @@ import random
 
 #Función de densidad de probabilidad f(x)=(1/32)*(8-x^3) con -2<= x <= 2.
 def aceptoRechazo(n):
-  rechazos = []
-  for i in range(n):
+  aceptados = []
+  rechazos = 0
+  while len(aceptados) < n:
     r1 = random.random()
     r2 = random.random()
     x = -2 + (2-(-2))*r1
     #g(x) = (1/32)*(8-x^3) / 0.5
-    if r2 > g(x):
-      rechazos.append(x)
-  return rechazos
+    if r2 <= g(x):
+      aceptados.append(x)
+    else:
+      rechazos += 1
+  return aceptados, rechazos
 
 def g(x):
-  return ((1/35)*(8-x**3))/0.5
+  return ((1/32)*(8-x**3))/0.5
 
-print(aceptoRechazo(1000))
+acepto,rechazo = aceptoRechazo(1000)
+
+print("Se aceptaron",len(acepto),"de 1000 observaciones")
+print("Se rechazaron", rechazo,"observaciones")
